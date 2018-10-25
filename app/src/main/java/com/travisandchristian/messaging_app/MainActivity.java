5ec36b2bf9fd3570a34247ef86230932cd10ee2d
tnabbefeld <tnabbefeld@gmail.com>
2018-10-25 01:50:30 -0500

    "made simple ui"

--- app/src/main/java/com/travisandchristian/messaging_app/MainActivity.java
+++ app/src/main/java/com/travisandchristian/messaging_app/MainActivity.java
@@ -2,6 +2,8 @@ package com.travisandchristian.messaging_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
